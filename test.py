1a40763347a14eca329b6bf599ee54602f5a2822
Mark Ono <60458022+the-mark-algorithm@users.noreply.github.com>
2021-08-28 23:43:55 -0500

    links to banks obtained!

--- test.py
+++ test.py
@@ -9,5 +9,9 @@ path = "/Users/markono/Desktop/chromedriver"
 browser = webdriver.Chrome(options=options, executable_path = path)
 browser.get('https://bankchart.us/catalogue/banks')
 content = browser.find_elements_by_class_name('abc-list_with-icon')
-print(len(content))
+
+for div in content:
+    link = div.find_element_by_tag_name('a')
+    href = link.get_attribute('href')
+    print(href)
 browser.close()
