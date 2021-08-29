a51b441fb70001e6bb2c1554643ccd9428bfbd72
Mark Ono <60458022+the-mark-algorithm@users.noreply.github.com>
2021-08-29 01:38:50 -0500

    very slow code

--- test.py
+++ test.py
@@ -10,8 +10,26 @@ browser = webdriver.Chrome(options=options, executable_path = path)
 browser.get('https://bankchart.us/catalogue/banks')
 content = browser.find_elements_by_class_name('abc-list_with-icon')
 
+
+links = []
+valid_websites = []
+
 for div in content:
     link = div.find_element_by_tag_name('a')
     href = link.get_attribute('href')
-    print(href)
+    links.append(href)
+
+for l in links:
+    browser.get(l)
+    bank_info = browser.find_elements_by_class_name('bank__info-item')
+
+for info in bank_info:
+    h5 = info.find_element_by_tag_name('h5')
+    if (h5.text == "Web-site:"):
+        website_link = info.find_element_by_tag_name('p').text
+        valid_websites.append(website_link)
+        print(website_link)
+
 browser.close()
+print(valid_websites)
+
